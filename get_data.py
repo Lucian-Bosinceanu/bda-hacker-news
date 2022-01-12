f363a49398754fe1698808c692b48fcadcf8f77f
Sergiu Iacob <sergiu.iacob1@gmail.com>
2022-01-12 09:20:40 +0200

    Update get_data.py script

--- get_data.py
+++ get_data.py
@@ -2,17 +2,27 @@ from google.cloud import bigquery
 import json
 import os
 
-
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="gcp_api_keys.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "gcp_api_keys.json"
 
 client = bigquery.Client()
+
+query = """
+    SELECT *
+    FROM fh-bigquery.hackernews.stories s
+    TABLESAMPLE SYSTEM (50 PERCENT)
+    WHERE
+        s.DEAD IS NOT true
+        AND s.TITLE != "None"
+        AND NOT (s.url = "NONE" AND s.text = "None")
+        AND rand() < 0.1
+"""
 
 query_join = """
     SELECT *
     FROM
         fh-bigquery.hackernews.stories s,
         fh-bigquery.hackernews.comments c
-    TABLESAMPLE SYSTEM (20 PERCENT)
+    TABLESAMPLE SYSTEM (50 PERCENT)
     WHERE
         c.parent = s.id
         AND s.DEAD IS NOT true
@@ -25,17 +35,23 @@ query_join = """
         AND rand() < 0.1
 """
 
-query_job = client.query(query_join) # Make an API request.
+
+def write_data(file, query_job):
+    print("starting to write data from", query_job)
+    with open(file, "w") as f:
+        for index, row in enumerate(query_job):
+            row_dict = dict(row)
+            del row_dict['time_ts']
+            if 'time_ts_1' in row_dict:
+                del row_dict['time_ts_1']
+            f.write(json.dumps(row_dict))
+            f.write("\n")
+            if index % 1000 == 0:
+                print("Wrote", index + 1)
 
 
-print("starting to write data from", query_job)
-with open("small_data_random.txt", "w") as f:
-    for index, row in enumerate(query_job):
-        row_dict = dict(row)
-        del row_dict['time_ts']
-        if 'time_ts_1' in row_dict:
-            del row_dict['time_ts_1']
-        f.write(json.dumps(row_dict))
-        f.write("\n")
-        if index % 1000 == 0:
-            print("Wrote", index + 1)
+query_job = client.query(query)
+write_data("small_data_random.txt", query_job)
+
+query_job = client.query(query_join)
+write_data("small_data_random_s_c.txt", query_job)
